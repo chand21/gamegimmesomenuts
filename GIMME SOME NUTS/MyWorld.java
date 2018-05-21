9459bcf84ff84077c9bcd901e3601902ecf6613b
uci <uciarsi12345@gmail.com>
2018-05-21 12:58:01 +0800

    GAME

--- GIMME SOME NUTS/MyWorld.java
+++ GIMME SOME NUTS/MyWorld.java
@@ -11,14 +11,10 @@ public class MyWorld extends World
 {
     
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class MyWorld
      * 
      */
-    counter skor = new counter ("Skor :");
-    public void tambahNilai()
-    {
-        skor.tambah(1);
-    }
+    
     public MyWorld()
     {    
         // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
